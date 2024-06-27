3289e62156ca9e89375fd3172c5c876aa9b2ab42
hein <hein.hpq@gmail.com>
2024-06-27 20:30:56 +0800

    feature: ThreadPoolConfig

--- core/src/main/java/cn/hein/core/config/ThreadPoolConfig.java
+++ core/src/main/java/cn/hein/core/config/ThreadPoolConfig.java
@@ -1,23 +1,32 @@
 package cn.hein.core.config;
 
-import cn.hein.core.listener.DynamicThreadPoolBeanListener;
-import cn.hein.core.properties.DynamicThreadPoolProperties;
+import cn.hein.core.listener.DynamicTpInitListener;
+import cn.hein.core.properties.DynamicTpProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 线程池配置类
+ * Configuration class for dynamic thread pools.
  *
  * @author hein
  */
+@Slf4j
 @Configuration
 public class ThreadPoolConfig {
 
+    /**
+     * Initializes a listener for dynamic thread pool setup if enabled in properties.
+     *
+     * @param dynamicTpProperties the dynamic thread pool properties
+     * @return DynamicTpInitListener if dynamic thread pools are enabled, otherwise null
+     */
     @Bean
-    public DynamicThreadPoolBeanListener dynamicThreadPoolBeanListener(DynamicThreadPoolProperties dynamicThreadPoolProperties) {
-        if (dynamicThreadPoolProperties.isEnable()) {
-            return new DynamicThreadPoolBeanListener(dynamicThreadPoolProperties);
+    public DynamicTpInitListener dynamicTpBeanListener(DynamicTpProperties dynamicTpProperties) {
+        if (dynamicTpProperties.isEnable()) {
+            return new DynamicTpInitListener(dynamicTpProperties);
         } else {
+            log.info("Dynamic thread pool initialization is disabled according to the configuration.");
             return null;
         }
     }
