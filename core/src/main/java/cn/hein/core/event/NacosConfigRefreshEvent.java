27b22e875aa2ed3e14f8825b3d11811eea7f6c8c
hein <hein.hpq@gmail.com>
2024-06-29 00:30:39 +0800

    optimize: rename class name

--- core/src/main/java/cn/hein/core/event/NacosConfigRefreshEvent.java
+++ core/src/main/java/cn/hein/core/event/NacosConfigRefreshEvent.java
@@ -5,22 +5,22 @@ import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
 /**
- * Event triggered when dynamic thread pool configurations are refreshed.
+ * Event triggered when nacos configurations are refreshed.
  *
  * @author hein
  */
 @Getter
-public class DynamicTpRefreshEvent extends ApplicationEvent {
+public class NacosConfigRefreshEvent extends ApplicationEvent {
 
     private final DynamicTpProperties message;
 
     /**
-     * Constructs a new DynamicTpRefreshEvent.
+     * Constructs a new NacosConfigRefreshEvent.
      *
      * @param source     the object on which the event initially occurred or with which the event is associated (never {@code null})
      * @param properties the dynamic thread pool properties that triggered the refresh (never {@code null})
      */
-    public DynamicTpRefreshEvent(Object source, DynamicTpProperties properties) {
+    public NacosConfigRefreshEvent(Object source, DynamicTpProperties properties) {
         super(source);
         this.message = properties;
     }
