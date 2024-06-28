9eb51e73bacd355e5f2cba88ec4815e2dc751a7e
hein <hein.hpq@gmail.com>
2024-06-29 00:39:09 +0800

    feature: steps to remove a transform kebab-case to camelCase

--- common/src/main/java/cn/hein/common/toolkit/YamlUtil.java
+++ common/src/main/java/cn/hein/common/toolkit/YamlUtil.java
@@ -2,50 +2,49 @@ package cn.hein.common.toolkit;
 
 import org.yaml.snakeyaml.Yaml;
 
-import java.util.*;
-
-import static cn.hein.common.toolkit.StringUtil.kebabCaseToCamelCase;
+import java.util.Map;
 
 /**
- * Utility class for yaml manipulation.
+ * Utility class for manipulating YAML data.
  *
- * @author hein
+ * @author Hein
  */
 public class YamlUtil {
 
     private static final Yaml instance = new Yaml();
 
     /**
-     * Converts the given YAML string content into an object instance of the specified class type.
+     * Converts a YAML string into an object of the specified type.
      *
-     * @param <T>     The type parameter of the target object.
-     * @param content The YAML formatted string content to be converted.
-     * @param clazz   The Class object of the target, specifying the type to convert to.
-     * @return An object instance converted, matching the type specified by clazz.
+     * @param content The YAML string to convert.
+     * @param clazz   The class type to which the YAML should be converted.
+     * @param <T>     The generic type of the class.
+     * @return An instance of the specified class populated with data from the YAML string.
      */
     public static <T> T convert(String content, Class<T> clazz) {
-        Map<String, Object> converted = new HashMap<>();
-        convertKeysToCamelCase(instance.load(content), converted);
-        return instance.loadAs(instance.dump(converted), clazz);
+        return instance.loadAs(content, clazz);
     }
 
     /**
-     * Converts the given YAML string content into an object instance of the specified class type,
-     * removing specified prefixes from all keys before conversion.
+     * Converts a YAML string into an object of the specified type, removing any prefixed keys.
      *
-     * @param <T>     The type parameter of the target object.
-     * @param content The YAML formatted string content to be converted.
-     * @param clazz   The Class object of the target, indicating the type to convert to.
-     * @param prefix  One or more camelCase strings representing prefixes to remove from keys.
-     *                If multiple prefixes are provided, all will be attempted to be removed from keys.
-     * @return A converted and processed object instance, conforming to the type specified by clazz.
+     * @param content The YAML string to convert.
+     * @param clazz   The class type to which the YAML should be converted.
+     * @param prefix  The prefixed keys to remove before conversion.
+     * @param <T>     The generic type of the class.
+     * @return An instance of the specified class populated with data from the YAML string.
      */
     public static <T> T convertWithoutPrefix(String content, Class<T> clazz, String... prefix) {
-        Map<String, Object> convertedMap = new HashMap<>();
-        convertKeysToCamelCase(instance.load(content), convertedMap);
-        return instance.loadAs(removePrefix(instance.dump(convertedMap), prefix), clazz);
+        return instance.loadAs(removePrefix(content, prefix), clazz);
     }
 
+    /**
+     * Removes prefixed keys from a YAML string.
+     *
+     * @param content The YAML string to process.
+     * @param prefix  The prefixed keys to remove.
+     * @return A String containing the YAML data without the prefixed keys.
+     */
     @SuppressWarnings("unchecked")
     private static String removePrefix(String content, String... prefix) {
         Map<String, Object> map = new Yaml().load(content);
@@ -61,34 +60,5 @@ public class YamlUtil {
         }
         return new Yaml().dump(map);
     }
-
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    private static void convertKeysToCamelCase(Map<String, Object> origin, Map<String, Object> converted) {
-        if (origin == null || converted == null) {
-            throw new IllegalArgumentException("Both 'origin' and 'converted' Maps must not be null.");
-        }
-        for (String key : origin.keySet()) {
-            Object value = origin.get(key);
-            String camelCaseKey = kebabCaseToCamelCase(key);
-            converted.put(camelCaseKey, value);
-            if (!key.equals(camelCaseKey)) {
-                converted.remove(key);
-            }
-            if (value instanceof Map nestedMap) {
-                convertKeysToCamelCase(nestedMap, (Map<String, Object>) converted.computeIfAbsent(camelCaseKey, k -> new LinkedHashMap<>()));
-            } else if (value instanceof List<?> list) {
-                List<Object> camelCaseList = new ArrayList<>(list.size());
-                list.forEach(each -> {
-                    if (each instanceof Map originalMap) {
-                        Map<String, Object> camelCaseEachMap = new LinkedHashMap<>();
-                        convertKeysToCamelCase(originalMap, camelCaseEachMap);
-                        camelCaseList.add(camelCaseEachMap);
-                    } else {
-                        camelCaseList.add(each);
-                    }
-                });
-                converted.put(camelCaseKey, camelCaseList);
-            }
-        }
-    }
 }
+
