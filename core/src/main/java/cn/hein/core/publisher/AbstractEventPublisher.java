02290142f10bb86c558b9addbf687dea1621ba51
hein <hein.hpq@gmail.com>
2024-06-29 00:37:07 +0800

    feature: abstract publisher and add collector for refresh event

--- core/src/main/java/cn/hein/core/publisher/AbstractEventPublisher.java
+++ core/src/main/java/cn/hein/core/publisher/AbstractEventPublisher.java
@@ -6,8 +6,7 @@ import org.springframework.context.ApplicationEventPublisher;
 
 /**
  * Abstract base class for event publishers that utilize Spring's ApplicationEventPublisher.
- * This class provides an abstract method to construct an ApplicationEvent from a given source object,
- * and a method to publish the constructed event using the provided ApplicationEventPublisher.
+ * This class provides methods to construct and publish events.
  *
  * @author hein
  */
@@ -25,14 +24,30 @@ public abstract class AbstractEventPublisher {
      * @param source The source object from which to build the event.
      * @return The constructed ApplicationEvent.
      */
-    protected abstract ApplicationEvent buildEvent(Object source);
+    protected abstract ApplicationEvent buildEvent(final Object source);
 
     /**
      * Publishes an event by first building it from the given source object, then publishing it.
      *
      * @param source The source object from which to build and publish the event.
      */
-    public void publishEvent(Object source) {
+    public void publishEvent(final Object source) {
+        beforePub();
         publisher.publishEvent(buildEvent(source));
+        afterPub();
+    }
+
+    /**
+     * Hook method called before the event is published.
+     */
+    protected void beforePub() {
+        // Default implementation does nothing.
+    }
+
+    /**
+     * Hook method called after the event has been published.
+     */
+    protected void afterPub() {
+        // Default implementation does nothing.
     }
 }
--- core/src/main/java/cn/hein/core/publisher/DynamicTpRefreshEventPublisher.java
+++ core/src/main/java/cn/hein/core/publisher/DynamicTpRefreshEventPublisher.java
@@ -1,21 +1,12 @@
 package cn.hein.core.publisher;
 
-import cn.hein.common.spring.ApplicationContextHolder;
-import cn.hein.common.toolkit.ThreadPoolInfoPrinter;
-import cn.hein.core.event.DynamicTpRefreshEvent;
-import cn.hein.core.executor.DynamicTpExecutor;
+import cn.hein.core.collector.DynamicTpCollector;
+import cn.hein.core.event.NacosConfigRefreshEvent;
 import cn.hein.core.properties.DynamicTpProperties;
-import com.alibaba.cloud.nacos.NacosConfigManager;
-import com.alibaba.nacos.api.config.listener.Listener;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 import static cn.hein.common.constant.executor.NacosConstant.DYNAMIC;
 import static cn.hein.common.constant.executor.NacosConstant.THREAD_POOL;
@@ -29,47 +20,26 @@ import static cn.hein.common.toolkit.YamlUtil.convertWithoutPrefix;
  */
 @Slf4j
 @Component
-public class DynamicTpRefreshEventPublisher extends AbstractEventPublisher implements InitializingBean {
+public class DynamicTpRefreshEventPublisher extends AbstractEventPublisher {
 
-    private final NacosConfigManager nacosConfigManager;
+    private final DynamicTpCollector collector;
 
-    @Value("${spring.cloud.nacos.config.name}")
-    private String dataId; // Nacos configuration file ID
-
-    @Value("${spring.cloud.nacos.config.group}")
-    private String group; // Nacos configuration group
-
-    public DynamicTpRefreshEventPublisher(ApplicationEventPublisher publisher, NacosConfigManager nacosConfigManager) {
+    public DynamicTpRefreshEventPublisher(ApplicationEventPublisher publisher, DynamicTpCollector collector) {
         super(publisher);
-        this.nacosConfigManager = nacosConfigManager;
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        nacosConfigManager.getConfigService().addListener(dataId, group, new Listener() {
-            @Override
-            public Executor getExecutor() {
-                // Returns a fixed-size thread pool for handling configuration update events
-                return Executors.newFixedThreadPool(1);
-            }
-
-            @Override
-            public void receiveConfigInfo(String configInfo) {
-                // When the configuration changes, publish a Dynamic ThreadPool refresh event
-                DynamicTpProperties message = convertWithoutPrefix(configInfo, DynamicTpProperties.class, DYNAMIC, THREAD_POOL);
-                message.getExecutors().forEach(executor -> executor.setBeanName(kebabCaseToCamelCase(executor.getThreadPoolName())));
-                System.out.println("before");
-                DynamicTpExecutor bean = ApplicationContextHolder.getBean(DynamicTpExecutor.class);
-                ThreadPoolInfoPrinter.printThreadPoolInfo(bean);
-                publishEvent(message);
-                System.out.println("after");
-                ThreadPoolInfoPrinter.printThreadPoolInfo(bean);
-            }
-        });
+        this.collector = collector;
     }
 
     @Override
     protected ApplicationEvent buildEvent(Object source) {
-        return new DynamicTpRefreshEvent(this, (DynamicTpProperties) source);
+        DynamicTpProperties message =
+                convertWithoutPrefix((String) source, DynamicTpProperties.class, DYNAMIC, THREAD_POOL);
+        message.getExecutors().forEach(executor ->
+                executor.setBeanName(kebabCaseToCamelCase(executor.getThreadPoolName())));
+        return new NacosConfigRefreshEvent(this, message);
+    }
+
+    @Override
+    protected void beforePub() {
+        collector.stop();
     }
 }
