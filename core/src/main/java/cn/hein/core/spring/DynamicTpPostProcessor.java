99fa5d3fa62ab8177af7e775de7946cde2c53051
hein <hein.hpq@gmail.com>
2024-07-01 21:42:10 +0800

    feature: DynamicTp Post Processor

--- core/src/main/java/cn/hein/core/spring/DynamicTpPostProcessor.java
+++ core/src/main/java/cn/hein/core/spring/DynamicTpPostProcessor.java
@@ -1,10 +1,17 @@
 package cn.hein.core.spring;
 
+import cn.hein.common.entity.properties.DynamicTpProperties;
+import cn.hein.common.entity.properties.ExecutorProperties;
 import cn.hein.core.DynamicTpContext;
 import cn.hein.core.executor.DynamicTpExecutor;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.lang.NonNull;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static cn.hein.common.spring.ApplicationContextHolder.getBean;
 
 /**
  * Post-processor for Spring Beans to intercept instances of DynamicTpExecutor and register them in the DynamicTpContext.
@@ -26,6 +33,12 @@ public class DynamicTpPostProcessor implements BeanPostProcessor {
         if (bean instanceof DynamicTpExecutor executor) {
             // Register the DynamicTpExecutor in the DynamicTpContext
             DynamicTpContext.registerDynamicTp(executor);
+            // handle allowCoreThreadTimeout property, maybe need to be optimized in the future
+            Map<String, ExecutorProperties> map = getBean(DynamicTpProperties.class).getExecutors()
+                    .stream().collect(Collectors.toMap(ExecutorProperties::getBeanName, v -> v));
+            if (map.containsKey(beanName) && map.get(beanName).isAllowCoreThreadTimeout()) {
+                executor.allowCoreThreadTimeOut(true);
+            }
             return executor;
         }
         return bean;
