efeb7a566d2e9f7a242e230613678c0623e89ae8
hein <hein.hpq@gmail.com>
2024-07-01 21:34:14 +0800

    optimize: optimize print content

--- common/src/main/java/cn/hein/common/toolkit/ThreadPoolInfoPrinter.java
+++ common/src/main/java/cn/hein/common/toolkit/ThreadPoolInfoPrinter.java
@@ -11,9 +11,12 @@ public class ThreadPoolInfoPrinter {
         System.out.println("Core Pool Size: " + executor.getCorePoolSize());
         System.out.println("Maximum Pool Size: " + executor.getMaximumPoolSize());
         System.out.println("Current Pool Size: " + executor.getPoolSize());
+        System.out.println("Largest Pool Size Ever: " + executor.getLargestPoolSize());
         System.out.println("Active Threads: " + executor.getActiveCount());
         System.out.println("Task Count (Total Queued & Running): " + executor.getTaskCount());
         System.out.println("Completed Task Count: " + executor.getCompletedTaskCount());
+        System.out.println("Allow CoreThread Timeout: " + executor.allowsCoreThreadTimeOut());
+        System.out.println("Keep Live Time: " + executor.getKeepAliveTime(TimeUnit.SECONDS) + "S");
 
         BlockingQueue<Runnable> queue = executor.getQueue();
         System.out.println("Queue Type: " + queue.getClass().getName());
@@ -23,8 +26,6 @@ public class ThreadPoolInfoPrinter {
         RejectedExecutionHandler handler = executor.getRejectedExecutionHandler();
         System.out.println("Rejected Execution Handler: " + handler.getClass().getName());
 
-        long largestPoolSize = executor.getLargestPoolSize();
-        System.out.println("Largest Pool Size Ever: " + largestPoolSize);
 
         System.out.println("--------------------------------------------------------");
     }
