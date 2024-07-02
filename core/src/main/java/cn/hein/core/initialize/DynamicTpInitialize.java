1e1904de61ac430ae0769f352509b506a5498bfb
hein <hein.hpq@gmail.com>
2024-07-02 23:38:28 +0800

    feature: DynamicTpInitialize for initMonitorExecutor

---
+++ core/src/main/java/cn/hein/core/initialize/DynamicTpInitialize.java
@@ -0,0 +1,36 @@
+package cn.hein.core.initialize;
+
+import cn.hein.common.entity.properties.DynamicTpProperties;
+import cn.hein.core.monitor.Monitor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * Initializes dynamic thread pool settings and starts the monitor if enabled.
+ *
+ * @author hein
+ */
+@Component
+@RequiredArgsConstructor
+public class DynamicTpInitialize implements InitializingBean {
+
+    private final DynamicTpProperties prop;
+    private final Monitor monitor;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        initMonitorExecutor(prop);
+    }
+
+    /**
+     * Checks if monitoring is enabled in the properties and starts the monitor if true.
+     *
+     * @param prop the properties object containing the monitor configuration.
+     */
+    private void initMonitorExecutor(DynamicTpProperties prop) {
+        if (prop.getMonitor().isEnabled()) {
+            monitor.start(prop);
+        }
+    }
+}
