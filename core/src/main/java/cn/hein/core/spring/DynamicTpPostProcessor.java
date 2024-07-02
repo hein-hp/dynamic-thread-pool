fafd9ffaec79614ce6b72bb51d6bd7f8ce5f47a5
hein <hein.hpq@gmail.com>
2024-07-02 23:29:02 +0800

    feature: add FilterRegisterPostProcessor and modify DynamicTpPostProcessor

--- core/src/main/java/cn/hein/core/spring/DynamicTpPostProcessor.java
+++ core/src/main/java/cn/hein/core/spring/DynamicTpPostProcessor.java
@@ -7,6 +7,7 @@ import cn.hein.core.executor.DynamicTpExecutor;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -14,20 +15,14 @@ import java.util.stream.Collectors;
 import static cn.hein.common.spring.ApplicationContextHolder.getBean;
 
 /**
- * Post-processor for Spring Beans to intercept instances of DynamicTpExecutor and register them in the DynamicTpContext.
+ * Post-processor for Spring Beans to intercept instances of DynamicTpExecutor and
+ * register them in the DynamicTpContext.
  *
  * @author hein
  */
+@Component
 public class DynamicTpPostProcessor implements BeanPostProcessor {
 
-    /**
-     * Processes beans after their initialization.
-     *
-     * @param bean     The initialized bean instance.
-     * @param beanName The name of the bean as defined in the bean definition.
-     * @return The bean instance, potentially wrapped or replaced by the post-processor.
-     * @throws BeansException If there is a problem with the processing of the bean.
-     */
     @Override
     public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
         if (bean instanceof DynamicTpExecutor executor) {
---
+++ core/src/main/java/cn/hein/core/spring/FilterRegisterPostProcessor.java
@@ -0,0 +1,26 @@
+package cn.hein.core.spring;
+
+import cn.hein.common.pattern.chain.Filter;
+import cn.hein.common.pattern.chain.FilterContext;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+/**
+ * Post-processor for Spring Beans to intercept instances of Filter and
+ * register them in the FilterContext.
+ *
+ * @author hein
+ */
+@Component
+public class FilterRegisterPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
+        if (bean instanceof Filter<?> filter) {
+            FilterContext.registerFilter(filter.getName(), filter);
+        }
+        return bean;
+    }
+}
