c5f438fa4433af674964aa09f8f5da20f984f72a
hein <hein.hpq@gmail.com>
2024-07-08 16:17:31 +0800

    feature: get proxy when init or refresh.

--- core/src/main/java/cn/hein/core/registry/DynamicTpBeanRegistry.java
+++ core/src/main/java/cn/hein/core/registry/DynamicTpBeanRegistry.java
@@ -7,6 +7,7 @@ import cn.hein.common.enums.executors.RejectionPolicyTypeEnum;
 import cn.hein.common.spring.BeanRegistryHelper;
 import cn.hein.common.toolkit.StringUtil;
 import cn.hein.core.executor.DynamicTpExecutor;
+import cn.hein.core.executor.reject.RejectHandlerProxyFactory;
 import cn.hein.core.support.PropertiesBindHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
@@ -15,6 +16,8 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.core.env.Environment;
 import org.springframework.lang.NonNull;
+
+import static cn.hein.common.enums.executors.RejectionPolicyTypeEnum.getRejectionPolicyEnum;
 
 /**
  * Registers dynamic thread pool beans based on configuration properties.
@@ -54,6 +57,7 @@ public class DynamicTpBeanRegistry implements BeanDefinitionRegistryPostProcesso
 
     private Object[] buildConstructorArgs(ExecutorProperties prop) {
         checkParam(prop);
+        RejectionPolicyTypeEnum originRejectionPolicy = getRejectionPolicyEnum(prop.getRejectedExecutionHandler());
         return new Object[]{
                 prop.getThreadPoolName(),
                 prop.getExecutorNamePrefix(),
@@ -62,7 +66,9 @@ public class DynamicTpBeanRegistry implements BeanDefinitionRegistryPostProcesso
                 prop.getKeepAliveTime(),
                 prop.getTimeUnit(),
                 BlockingQueueTypeEnum.getBlockingQueue(prop.getQueueType(), prop.getQueueCapacity()),
-                RejectionPolicyTypeEnum.getRejectionPolicy(prop.getRejectedExecutionHandler())
+                // getProxy
+                RejectHandlerProxyFactory.getProxy(originRejectionPolicy),
+                originRejectionPolicy
         };
     }
 
